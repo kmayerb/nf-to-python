b573a579fa8aa0e7ff2e664bac70b0dd32a47dd4
kmayerb <kmayerb@gmail.com>
2021-11-05 16:24:11 -0700

    add output folder

--- run_script.sh
+++ run_script.sh
@@ -16,7 +16,7 @@ QUEUE=campus-new
 # Workflow-Specific Options
 
 RESOURCES=/home/kmayerbl/NextFlow/nf-to-python/test_data/
-OUTPUT_FOLDER=/home/kmayerbl/NextFlow/nf-to-python/test_data/test_data/test_output_folder/
+OUTPUT_FOLDER=/fh/fast/gilbert_p/kmayerbl/nextflow_sink/
 
 # Load the Nextflow module (if running on rhino/gizmo)
 ml Nextflow
